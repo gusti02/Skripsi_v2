9502bae75303ab6af25520c0e8301025bc260c76
Gusti <gustirizki26@gmail.com>
2024-04-13 10:52:57 +0700

    add icon chart on navbar

--- frontend/my-react-app/src/components/Layout/Navbar.jsx
+++ frontend/my-react-app/src/components/Layout/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../Elements/Button";
 import { useLogin } from "../../hooks/useLogin";
+import { CiShoppingCart } from "react-icons/ci";
+import { useSelector } from "react-redux";
 
 function Navbar() {
   const username = useLogin();
+  const [totalCart, setTotalCart] = useState(0); // state the total cart
+  const cart = useSelector((state) => state.cart.data);
+
+  useEffect(() => {
+    const sum = cart.reduce((accumulator, item) => {
+      return accumulator + item.qty;
+    }, 0);
+    setTotalCart(sum);
+  }, [cart]);
 
   {
     /* Handler Logout, after click logout will redirect to login and remove token */
@@ -19,6 +30,12 @@ function Navbar() {
       <Button className="bg-black ml-5" onClick={handleLogout}>
         Logout
       </Button>
+      <div className="flex item-center bg-gray-800 p-2 rounded-md ml-5">
+        <CiShoppingCart size={20} />
+        <div className="absolute ml-2.5 mt-1.5 bg-red-700 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+          {totalCart}
+        </div>
+      </div>
     </div>
   );
 }
